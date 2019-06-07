dae9b1918052c639a3127b1ba0895493cee86526
gzm1997 <1617899539@qq.com>
2019-06-07 11:06:58 +0800

    使用动态代理的方法编写一个日志类。

--- src/cn/gzm/dynamicProxy/proxy/LogProxy.java
+++ src/cn/gzm/dynamicProxy/proxy/LogProxy.java
@@ -1,20 +1,16 @@
 package cn.gzm.dynamicProxy.proxy;
 
-import cn.gzm.staticProxy.dao.Rent;
-
-import java.lang.annotation.Target;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.sql.ResultSet;
 
 /**
  * @author: gzm
  * @date: 19-6-6
  * @description: Pass
  */
-public class ProxyMen implements InvocationHandler {
+public class LogProxy implements InvocationHandler {
     private Object target;
 
     public void setTarget(Object target) {
@@ -22,14 +18,18 @@ public class ProxyMen implements InvocationHandler {
     }
 
     /*
-    * 生成代理类*/
+    * 生成一个代理类的实例*/
     public Object getProxy() {
         return Proxy.newProxyInstance(this.getClass().getClassLoader(), target.getClass().getInterfaces(), this);
     }
-
+    /*
+    * 在代理对象中执行被引用的方法，
+    * 注意：这个方法会在当被代理方法的某个方法method被调用的时候进行调用，
+    * 返回被代理对象特定方法的返回值*/
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) {
         Object result;
+        log(method.getName());
         try {
             result = method.invoke(this.target, args);
         } catch (InvocationTargetException e) {
@@ -40,5 +40,8 @@ public class ProxyMen implements InvocationHandler {
             return null;
         }
         return result;
+    }
+    private void log(String methohName) {
+        System.out.println("日志:正在执行" + methohName + "函数");
     }
 }
--- src/cn/gzm/dynamicProxy/test/Test.java
+++ src/cn/gzm/dynamicProxy/test/Test.java
@@ -2,7 +2,10 @@ package cn.gzm.dynamicProxy.test;
 
 import cn.gzm.dynamicProxy.dao.User;
 import cn.gzm.dynamicProxy.dao.impl.UserImpl;
-import cn.gzm.dynamicProxy.proxy.ProxyMen;
+import cn.gzm.dynamicProxy.proxy.LogProxy;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author: gzm
@@ -12,9 +15,14 @@ import cn.gzm.dynamicProxy.proxy.ProxyMen;
 public class Test {
     public static void main(String[] args) {
         User user = new UserImpl();
-        ProxyMen proxyMen = new ProxyMen();
-        proxyMen.setTarget(user);
-        User proxyUser = (User) proxyMen.getProxy();
+        LogProxy logProxy = new LogProxy();
+        logProxy.setTarget(user);
+        User proxyUser = (User) logProxy.getProxy();
         proxyUser.walk();
+
+        List<String> list = new ArrayList<>();
+        logProxy.setTarget(list);
+        List<String> listProxy = (List<String>) logProxy.getProxy();
+        listProxy.add("lala");
     }
 }
